f5ee0c96e26624c9edc23db420fb4a2ccf97d579
PanYJ <1025318325@qq.com>
2018-09-22 16:38:00 +0800

    empty

---
+++ cmake-build-debug/CMakeFiles/ring_LWE.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ring_LWE.dir/luts.c.obj"
+  "CMakeFiles/ring_LWE.dir/lwe.c.obj"
+  "CMakeFiles/ring_LWE.dir/lwe_original.c.obj"
+  "CMakeFiles/ring_LWE.dir/main.c.obj"
+  "ring_LWE.pdb"
+  "ring_LWE.exe"
+  "ring_LWE.exe.manifest"
+  "libring_LWE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ring_LWE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ring_LWE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ring_LWE.
+# This may be replaced when dependencies are built.
